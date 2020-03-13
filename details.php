db3e933a4f7ebf22ea800defe0b5aa012aa79a5e
Travis Edward <Travis Edward@TECH-PC>
2020-03-13 17:24:35 -0400

    Add Breadcrumbs and Details page edit
    Created breadcrumbs for page changes. Due to one database and page limit
    the product type in breadcrumb directs the customer to the
    index.php(product list page). Added Functions to only display the sale
    percentage on the item which has a sale or which sale price is not
    higher than the actual price. Added php statement in details page to
    display only the products price if the sale is equal to zero and put a
    line through the maine price if there is a sale.
    removed header and footer additional pages.

--- details.php
+++ details.php
@@ -12,9 +12,67 @@ if(isset($_GET["pid"])){
 }
 
 
-include 'header.php'; ?>
+?>
 
-<?php include 'connect_db.php';
+ <html>
+    <head>
+        <title>Products Home</title>
+          <link href="style.css" rel="stylesheet" >
+        <!---script to add icons to page-->
+            <script src="https://kit.fontawesome.com/bc9aeacf84.js" crossorigin="anonymous"></script>
+    </head>
+        <body>
+
+          <header class="main-header">
+            <!--image logo with link to home(index) page-->  
+              <div>
+             <a href="index.php">
+            <img src=" images/logo.png" class="logo" >
+            </a>
+          </div>
+
+        <!--- search box in a div container-->
+          <div class="search-box">
+            <input class="search-text" type="text" name=" " placeholder="Search for Product">
+              <a class="search-btn "href="#">
+                <i class="fas fa-search"></i>
+              </a>
+          </div>
+
+          <ul class="main-nav">
+            <li><a href="homepage.php"><i class="fas fa-home"></i> HOME</a></li>
+            <li class="dropdown">
+
+              <a href="index.php"><i class="fab fa-shopify"></i> PRODUCTS <i class="fa fa-caret-down"></i></a>
+              <ul class="drop-nav">
+                <li class="flyout">
+                  <a href="#">TOP-UP</a>
+                   <ul class="flyout-nav">
+                    <li><a href="#">Digicel</a></li>
+                    <li><a href="#">Bmobile</a></li>        
+                  </ul>
+                </li>
+                <li class="flyout">
+                  <a href="#">Phones</a>
+                  <ul class="flyout-nav">
+                    <li><a href="#">iPhone</a></li>
+                    <li><a href="#">Apple</a></li>
+                    <li><a href="#">Samsung</a></li>        
+                  </ul>
+                </li>    
+              </ul>
+            </li>
+            <li><a href="#"><i class="fas fa-info-circle"></i> ABOUT</a></li>
+          </ul>
+        </header>
+
+
+
+<!-- breadcrumb for navigation -->
+
+<?php 
+
+  $dbconnect = mysqli_connect("localhost","root","","topcellersdb") OR die(mysqli_connect_error());
 
   //access products table from database
   $sql ="SELECT * FROM `products` WHERE products.product_id = $product_id";
@@ -28,7 +86,66 @@ include 'header.php'; ?>
             while($row = mysqli_fetch_array($query,MYSQLI_ASSOC)){
               //create variables for each field
 
-            	  $product_id = $row["product_id"];
+                $product_id = $row["product_id"];
+                  $product_name =$row["product_name"];
+                  $product_type = $row["product_type"];
+
+?>
+
+          <ul class="breadcrumb">
+          <li class="breadcrumb-item">
+              <a href="homepage.php" class="breadcrumb-link">Home</a>
+          </li>
+          <li class="breadcrumb-item">
+              <a href="index.php" class="breadcrumb-link">Products</a>
+          </li>
+          <li class="breadcrumb-item">
+              <a href="index.php" class="breadcrumb-link"><?php echo $product_type; ?></a>
+          </li>
+          <li class="breadcrumbs-item">
+              <a href="" class="breadcrumb-link-active"><?php echo $product_name; ?></a>
+          </li>
+            </ul>
+
+
+<?php
+                }
+                } else {echo "Site is under maintenance";}
+            ?>
+
+<br>
+<br>
+<!------end of bread crumb ----->
+
+
+
+
+
+
+<div class="detail_box">
+
+<h1 style="text-align: center;"><?php echo $product_name; ?></h1>
+
+<ul>
+  
+<?php 
+
+  $dbconnect = mysqli_connect("localhost","root","","topcellersdb") OR die(mysqli_connect_error());
+
+
+  //access products table from database
+  $sql ="SELECT * FROM `products` WHERE products.product_id = $product_id";
+
+      //create query to execute sql on database
+      $query = mysqli_query($dbconnect, $sql);
+
+    //statement to display day under condition
+          if($query){
+            //loop each row
+            while($row = mysqli_fetch_array($query,MYSQLI_ASSOC)){
+              //create variables for each field
+
+            	    $product_id = $row["product_id"];
                   $product_name =$row["product_name"];
                   $product_details = $row["product_details"];
                   $product_image = $row["product_image"];
@@ -36,24 +153,67 @@ include 'header.php'; ?>
                   $sale_price = $row["sale_price"];
                   $product_type = $row["product_type"];
 
-?>
+          ?>
 
-    <li>
-      <img class="responsive" src=images/<?php echo $product_image;?> alt="image">
-        <p class="info"><?php echo $product_name; ?></a></p>
-        <p class="detail"> <?php echo $product_details; ?></p> 
-        <h5 class="info">Type: <?php echo $product_type; ?></h5>
-        <h5 class="info">Price: $<?php echo $product_price; ?> TTD</h5>
-    </li>
+              
+                <img class="detail_image" src=images/<?php echo $product_image;?> alt="image">
+                <li><p class="info"><?php echo $product_name; ?></a></p>
+                  <h5 class="info">Type: <?php echo $product_type; ?></h5>
+                  <p class="detail"> <?php echo $product_details; ?></p> 
+                  
+       <!-------- cross out price if there is a sale---->
+                  <?php 
 
-<?php
+                    if($sale_price <> 0 ){
 
-    }
+                    echo '<h2 class="info" style="text-decoration:line-through;"> $'.$product_price.' TTD </h2>';
 
-      } else {
-              echo "Site is under maintenance";
-}
+                  }else{
+                    echo '<h2 class="info" style="text-decoration:none;"> $'.$product_price.'TTD </h2>'; 
+                    
+                  }
+
+                  ?>
 
 
+        <!----remove sale price when value is 0 ------>
 
-include 'footer.php'; ?>
+                  <h4 class="info">
+
+                    <?php 
+
+                    if($sale_price != 0){
+
+                    echo '$'.$sale_price.' TTD'; 
+
+                      }else{
+
+                        echo "";
+                      }
+
+                      ?>
+                    </h4>
+              </li>
+
+          <?php
+
+              }
+              } else {echo "Site is under maintenance";}
+          ?>        
+</ul>
+
+</div>
+
+          <footer class="footer-nav">
+            <nav>
+              <div class="wrapper">
+                <ul>
+                  <li><a href="index.php">HOME</a></li>
+                  <li><a href="about.php">ABOUT</a></li>
+                  <li><a href="products.php">PRODUCTS</a></li>
+                </ul>
+              </div>
+            </nav>  
+        </footer>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,29 +1,88 @@
 
-
 <?Php
 
 session_start();
 
-include "header.php"; ?>
+?>
 
-  <ul class="breadcrumb">
-  <li class="breadcrumb-item">
-    <a href="#" class="breadcrumb-link">Home</a>
-  </li>
-  <li class="breadcrumb-item">
-    <a href="#" class="breadcrumb-link">Products</a>
-  </li>
-  <li class="breadcrumbs-item">
-    <a href="#" class="breadcrumb-link-active">iPhone</a>
-  </li>
-</ul>
+<html>
+<head>
+  <title>Products Home</title>
+  <link href="style.css" rel="stylesheet" >
+  <!---script to add icons to page-->
+  <script src="https://kit.fontawesome.com/bc9aeacf84.js" crossorigin="anonymous"></script>
+</head>
+<body>
 
-  <h1 style="text-align: center;">Product Listing</h1>
-    <p style="text-align: center;"> Shop and see some of our best deals!</p>
+  <header class="main-header">
+    <!--image logo with link to home(index) page-->  
+      <div>
+     <a href="index.php">
+    <img src=" images/logo.png" class="logo" >
+    </a>
+  </div>
 
+  <!--- search box in a div container-->
+    <div class="search-box">
+      <input class="search-text" type="text" name=" " placeholder="Search for Product">
+        <a class="search-btn "href="#">
+          <i class="fas fa-search"></i>
+        </a>
+    </div>
+
+    <ul class="main-nav">
+      <li><a href="homepage.php"><i class="fas fa-home"></i> HOME</a></li>
+      <li class="dropdown">
+
+        <a href="index.php"><i class="fab fa-shopify"></i> PRODUCTS <i class="fa fa-caret-down"></i></a>
+        <ul class="drop-nav">
+          <li class="flyout">
+            <a href="#">TOP-UP</a>
+             <ul class="flyout-nav">
+              <li><a href="#">Digicel</a></li>
+              <li><a href="#">Bmobile</a></li>        
+            </ul>
+          </li>
+          <li class="flyout">
+            <a href="#">Phones</a>
+            <ul class="flyout-nav">
+              <li><a href="#">iPhone</a></li>
+              <li><a href="#">Apple</a></li>
+              <li><a href="#">Samsung</a></li>        
+            </ul>
+          </li>    
+        </ul>
+      </li>
+      <li><a href="#"><i class="fas fa-info-circle"></i> ABOUT</a></li>
+    </ul>
+  </header>
+
+<!---- breadcrumb ---->
+        <ul class="breadcrumb">
+          <li class="breadcrumb-item">
+              <a href="homepage.php" class="breadcrumb-link">Home</a>
+          </li>
+          <li class="breadcrumb-item">
+              <a href="index.php" class="breadcrumb-link-active">Products</a>
+          </li>
+        </ul>
+    <br>
+<br>
+<!---- breadcrumb end ---->
+
+
+
+<!---------------------------- product listing ----------------------------->
+
+<div class="product_box">
+  <h1>Product Listing</h1>
+<p style="text-align: center;">Shop for some of the best deals!</p>
 <ul class="products">
 
-<?php include 'connect_db.php';
+
+<?php 
+
+$dbconnect = mysqli_connect("localhost","root","","topcellersdb") OR die(mysqli_connect_error());
 
   //access products table from database
   $sql ="SELECT * FROM `products`";
@@ -46,26 +105,60 @@ include "header.php"; ?>
                   $product_type = $row["product_type"];
                   $link = "details.php?pid=$product_id";
 
-?>
+                  $sale = (($sale_price/$product_price) * 100);
 
+                  
+                  
+
+
+?>
     <li>
       <img class="responsive" src=images/<?php echo $product_image;?> alt="image">
         <p class="info"><a href="<?php echo $link ?>" class="btn-card"><?php echo $product_name; ?></a></p>
-        <h5 class="info">Type: <?php echo $product_type; ?></h5>
-        <h5 class="info">Price: $<?php echo $product_price; ?> TTD</h5>
+        <h2 class="info">Price: $<?php echo $product_price; ?> TTD</h2>
+        <h4 class="info" style="color:red;">
+
+
+          <?php 
+
+//check if product has a discout and display with code to prevent displaying over 100% sale.
+          if($product_price < $sale_price) {
+
+                    echo "";
+
+          }elseif($sale == 0){
+
+                    echo "";
+
+                  }else{
+
+                    echo "$".round($sale)."% OFF"; 
+                  }
+
+          ?>
+
+        </h4>
     </li>
 
 <?php
-
-          /*<p class="detail"> <?php echo $product_details; ?></p> */
-
-        }
-
-      } else {
-              echo "Site is under maintenance";
-}
-
+    }
+    } else {echo "Site is under maintenance";}
 ?>
 </ul>
+</div>
 
-<?php include 'footer.php'; ?>
+
+<footer class="footer-nav">
+  <nav>
+    <div class="wrapper">
+      <ul>
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="about.php">ABOUT</a></li>
+        <li><a href="products.php">PRODUCTS</a></li>
+      </ul>
+    </div>
+  </nav>
+    
+</footer>
+</body>
+</html>
