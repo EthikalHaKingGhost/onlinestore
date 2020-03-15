3fe12a4c1fba11948d46f046d4f802be5553863e
Travis Edward <Travis Edward@TECH-PC>
2020-03-15 19:37:47 -0400

    CSS for image slider
    Created CSS structure for the slider to look as expected. borders over
    images with thumbnails showing as cards on the left side and larger
    image next to selector.

--- test.php
+++ test.php
@@ -5,7 +5,56 @@
 </head>
 <body>
 
+	<style type="text/css">
+		body{
+			margin:0px;
+			padding:0px;
+		}
 
+		.container{
+			position:absolute;
+			top: 10%;
+			float:left;
+			padding-left:20px;
+			float:left;
+			overflow: hidden;
+		}
+
+		ul.thumb{
+			margin:0 auto;
+			padding: 0;
+			float: left;
+		}
+
+		ul.thumb li{
+			list-style: none;
+			margin: 5px;
+			padding:4px;
+			width:auto;
+			height:auto;
+			border:1px solid rgba(0,0,0,.2);
+			overflow: hidden;
+		}
+
+		ul.thumb li img {
+			width:100%;
+		}
+
+		.imgBox{
+			float:left;
+			width:500px;
+			height:500px;
+			margin:6px;
+			border:1px solid rgba(0,0,0,.2);
+			overflow: hidden;
+		}
+
+		.imgBox img{
+			width: 100%;
+		}
+	
+
+	</style>
 
 	<div class="container">
 		<ul class="thumb">
@@ -19,6 +68,5 @@
 		</ul>
 		<div class="imgBox"><img src="images/Apple/4/Apple-iPhone-11-pro-max.jpg"></div>
 	</div>
-
 </body>
 </html>
