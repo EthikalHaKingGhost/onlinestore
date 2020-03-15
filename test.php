ba7c965d4a39df01c72b8849501a7dd880b71ed9
Travis Edward <Travis Edward@TECH-PC>
2020-03-15 19:32:37 -0400

    Product image slider for details page
    Creating an image slider. Added div and ul tags to create structure.
    target tags are added to thumbnails to display the larger image on
    click. display shows three thumbnails with a larger image.

---
+++ test.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+
+	<div class="container">
+		<ul class="thumb">
+
+			<li><a href="images/Apple/4/img1.jpg" target="imgBox"><img src="images/Apple/4/thumb1.jpg"></a></li>
+
+			<li><a href="images/Apple/4/img2.jpg" target="imgBox"><img src="images/Apple/4/thumb2.jpg"></a></li>
+
+			<li><a href="images/Apple/4/img3.jpg" target="imgBox"><img src="images/Apple/4/thumb3.jpg"></a></li>
+
+		</ul>
+		<div class="imgBox"><img src="images/Apple/4/Apple-iPhone-11-pro-max.jpg"></div>
+	</div>
+
+</body>
+</html>
